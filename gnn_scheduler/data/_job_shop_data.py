e674981e603c86dd696e790d494e23c7c994100f
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-05-29 21:15:03 +0100

    node types fixed for airplane scheduling

--- gnn_scheduler/data/_job_shop_data.py
+++ gnn_scheduler/data/_job_shop_data.py
@@ -19,7 +19,7 @@ class JobShopData(HeteroData):
             # Set increments for each column according to respective node
             # counts
             # TODO: insert the list of nodes for airplane scheduling
-            node_types = ["operation", "machine", "job"]
+            node_types = ["workorder", "technician", "workpackage"]
             for i, node_type in enumerate(node_types):
                 if self[node_type]:
                     increments[i] = self[node_type]["x"].size(0)
