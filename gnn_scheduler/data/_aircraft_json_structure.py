6c3405aa527ff0f3757c2981fa9cc544681f8828
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-06-25 16:22:07 +0100

    function defined for parsing CSV and writing JSON file

--- gnn_scheduler/data/_aircraft_json_structure.py
+++ gnn_scheduler/data/_aircraft_json_structure.py
@@ -1,2 +1,13 @@
 import csv
 import json
+
+
+def parse_files(work_packages_file, aircrafts_file):
+
+    # Mpaping Resources(Technicians) to their IDs
+    staff_index_map = {
+        "B1 Technician": 1,
+        "B2 Technician": 2,
+        "B1 Engineer": 3,
+        "B2 Engineer": 4,
+    }
